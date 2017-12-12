ee1328cd3c5956d71d18ba49026ae36f4c1b6596
José de Figueiredo <deb.gnulinux@gmail.com>
2017-12-12 12:32:40 -0200

    segundo commit

--- ColetorCorrente.cpp
+++ ColetorCorrente.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 
+byte led = 13;
+boolean estado = false;
+
 void setup() {
 	Serial.begin(9600);
+	pinMode(led,OUTPUT);
 	//comentario para alterar o arquviso
 }
 
 void loop() {
-
+	estado = !estado;
+	digitalWrite(led,estado);
+	delay(1000);
 }
